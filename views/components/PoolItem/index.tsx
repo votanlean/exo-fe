620a9ef7b6cc51fb90f40da9a351a63a1ab1be1e
An Vo <votanlean@gmail.com>
2021-05-12 12:39:26 +0700

    Update symbol and address

--- views/components/PoolItem/index.tsx
+++ views/components/PoolItem/index.tsx
@@ -3,15 +3,12 @@ import web3 from '../../../binance/web3'
 import { useEffect, useState } from 'react'
 import tEXOTokenInstance from 'binance/tEXOToken'
 import orchestratorInstance from 'binance/orchestrator'
-// import tUSDTInstance from 'binance/tUSDT'
-
 
 function PoolItem({ data }) {
   const {
     id,
     icon,
     title,
-    apr,
     tokenInstance,
     symbol,
   } = data || {}
@@ -21,26 +18,20 @@ function PoolItem({ data }) {
   const [currentReward, setCurrentReward] = useState(0)
   const [walletBalance, setWalletBalance] = useState(0)
 
-
-  const tEXOTokenAddress = '0x3bbabe10348e557690c41794392e811d41d18511';
   const orchestratorAddress = '0x7c3203Bc44e6b49c3cbfBc0F472Ae35E3aa23012';
-  const tUSDTAddress = '0x337610d27c682E347C9cD60BD4b3b107C9d34dDd';
-  // const USDTPool = '0x30eabfa41c69dbe986a5f3513948a4660ea17d97';
-  console.log('tEXOTokenAddress', tEXOTokenAddress);
   const handleClickApprove = async () => {
     const accounts = await web3.eth.getAccounts();
-    console.log('account staker', accounts);
     tokenInstance.methods.approve(orchestratorAddress, web3.utils.toWei('8', 'ether')).send({ from: accounts[0] });
   }
 
   const handleClickStake = async () => {
     const accounts = await web3.eth.getAccounts();
-    orchestratorInstance.methods.deposit(0, web3.utils.toWei('1', 'ether')).send({ from: accounts[0] });
+    orchestratorInstance.methods.deposit(id, web3.utils.toWei('1', 'ether')).send({ from: accounts[0] });
   }
 
   const handleClickClaimRewards = async () => {
     const accounts = await web3.eth.getAccounts();
-    orchestratorInstance.methods.deposit(0, 0).send({ from: accounts[0] });
+    orchestratorInstance.methods.deposit(id, 0).send({ from: accounts[0] });
   }
 
 
@@ -51,7 +42,6 @@ function PoolItem({ data }) {
       const accounts = await web3.eth.getAccounts();
       if (accounts[0]) {
         const allowance = await tEXOTokenInstance.methods.allowance(accounts[0], orchestratorAddress).call();
-        console.log('allowance', allowance);
       }
 
     }
@@ -61,7 +51,6 @@ function PoolItem({ data }) {
       const accounts = await web3.eth.getAccounts();
       if (accounts[0]) {
         const totalSupply = await tokenInstance.methods.balanceOf(orchestratorAddress).call();
-        console.log('totalSupply', totalSupply);
         setTotalSupply(totalSupply / Math.pow(10, 18));
       }
     }
@@ -70,8 +59,7 @@ function PoolItem({ data }) {
     const getMyStake = async () => {
       const accounts = await web3.eth.getAccounts();
       if (accounts[0]) {
-        const myStake = await orchestratorInstance.methods.userInfo(0, accounts[0]).call();
-        console.log('myStake', myStake[0]);
+        const myStake = await orchestratorInstance.methods.userInfo(id, accounts[0]).call();
         setMyStake(myStake[0] / Math.pow(10, 18));
       }
     }
@@ -81,7 +69,6 @@ function PoolItem({ data }) {
       const accounts = await web3.eth.getAccounts();
       if (accounts[0]) {
         const walletBalance = await tokenInstance.methods.balanceOf(accounts[0]).call();
-        console.log('walletBalance', walletBalance);
         setWalletBalance(web3.utils.fromWei(walletBalance, 'ether'));
       }
     }
@@ -91,7 +78,6 @@ function PoolItem({ data }) {
       const accounts = await web3.eth.getAccounts();
       if (accounts[0]) {
         const currentReward = await orchestratorInstance.methods.pendingTEXO(0, accounts[0]).call();
-        console.log('currentReward', currentReward);
         setCurrentReward(web3.utils.fromWei(currentReward, 'ether'));
       }
     }
@@ -123,7 +109,7 @@ function PoolItem({ data }) {
                   className={`d-flex items-center justify-between font-bold ${styles.colorLight}`}
                 >
                   <p className={styles.pTitle}>My Rewards</p>
-                  <p>{currentReward} TEXO</p>
+                  <p>{currentReward} tEXO</p>
                 </div>
                 <div className="d-flex items-center justify-between">
                   <p className={styles.pTitle}>Total Staked</p>
