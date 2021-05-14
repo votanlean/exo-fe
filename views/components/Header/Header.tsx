b307f0634076d74cbb5b8d69bb94cfb716c9fa8a
An Vo <votanlean@gmail.com>
2021-05-14 10:31:28 +0700

    Create logout and handle click outside

--- views/components/Header/Header.tsx
+++ views/components/Header/Header.tsx
@@ -1,19 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Link from 'next/link'
 import { useWeb3React } from '@web3-react/core'
 import { getErrorMessage } from '~lib/error'
 import Nav from '../Nav/Nav'
 import styles from './header.module.scss'
-import {metamask, bsc} from '~lib/connector'
+import { metamask, bsc } from '~lib/connector'
 
 const Header = () => {
   const [openPopup, setOpenPopup] = useState(false)
-  const { error} = useWeb3React()
+  const [openLogoutPopup, setOpenLogoutPopup] = useState(false)
+  const { error } = useWeb3React()
 
   const { chainId, account, activate, deactivate, active } = useWeb3React()
 
-  const onClickConnect = async () => {
+  //Handle Connect
+  const onClickConnect = () => {
     setOpenPopup(true)
+  }
+
+  const handleClosePopup = () => {
+    setOpenPopup(false)
   }
 
   const handleConnectMetamask = async () => {
@@ -22,24 +28,49 @@ const Header = () => {
   }
 
   const handleConnectBinanceChainWallet = async () => {
-    await activate(bsc);
-
+    await activate(bsc)
     setOpenPopup(false)
   }
 
-  const onDeactivate = async () => {
+  //Handle Logout
+  const onDeactivate = () => {
+    setOpenLogoutPopup(true)
+  }
+
+  const handleCloseLogoutPopup = () => {
+    setOpenLogoutPopup(false)
+  }
+
+  const onClickLogout = () => {
     deactivate()
+    setOpenLogoutPopup(false)
   }
 
-  const handleClosePopup = () => {
-    setOpenPopup(false)
-  }
-
+  //If have error
   useEffect(() => {
     if (error) {
       alert(getErrorMessage(error))
     }
   }, [error])
+
+  //Handle click outside when have popup
+  const useOutside = ref => {
+    useEffect(() => {
+      const handleClickOutside = () => {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setOpenPopup(false)
+          setOpenLogoutPopup(false)
+        }
+      }
+      document.addEventListener('mousedown', handleClickOutside)
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside)
+      }
+    }, [ref])
+  }
+
+  const wrapperRef = useRef(null)
+  useOutside(wrapperRef)
 
   return (
     <>
@@ -70,8 +101,9 @@ const Header = () => {
         </div>
       </header>
 
+      {/* Connect Popup */}
       {openPopup && (
-        <div className={styles.connectPopup}>
+        <div className={styles.connectPopup} ref={wrapperRef}>
           <div className={styles.popupHeader}>
             <h2>Connect to wallet</h2>
             <button onClick={handleClosePopup}>
@@ -92,6 +124,17 @@ const Header = () => {
                 src="/static/images/binance-chain-wallet-logo.jpeg"
                 alt="binance-chain-wallet-logo"
               />
+            </button>
+          </div>
+        </div>
+      )}
+
+      {/* Logout Popup */}
+      {openLogoutPopup && (
+        <div className={styles.connectPopup} ref={wrapperRef}>
+          <div className={styles.popupMain}>
+            <button className={styles.logoutButton} onClick={onClickLogout}>
+              <h3>Log out</h3>
             </button>
           </div>
         </div>
