eaf7a8b87cc9d0b8d42688919f35fc632b914144
An Vo <votanlean@gmail.com>
2021-05-17 14:37:52 +0700

    Coming Soon

---
+++ pages/mint/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import ComingSoon from '../../views/components/ComingSoon'
+import React from 'react'
+
+function Mint() {
+  return (
+    <>
+      <Head>
+        <title>Mint</title>
+      </Head>
+      <ComingSoon/>
+    </>
+  )
+}
+
+export default Mint
