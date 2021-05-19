40b4bbaedb49e542774e3764de66060643b859bd
An Vo <votanlean@gmail.com>
2021-05-19 11:45:38 +0700

    Fix mui nextjs bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,32 @@
-import App from 'next/app'
-import { Web3ReactProvider } from '@web3-react/core';
 import { ApolloProvider } from '@apollo/react-hooks'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { Web3ReactProvider } from '@web3-react/core'
+import React, { useEffect } from 'react'
 import apollo from '~lib/apolloClient'
-import { getLibrary } from '~lib/web3';
-import MainLayout from './layout'
+import { getLibrary } from '~lib/web3'
 import '~styles/main.scss'
+import MainLayout from './layout'
+import theme from './theme/theme'
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
-		return (
-				<Web3ReactProvider getLibrary={getLibrary}>
-					<ApolloProvider client={apollo}>
-							<MainLayout>
-								<Component {...pageProps} />
-							</MainLayout>
-					</ApolloProvider>
-				</Web3ReactProvider>
-    )
-  }
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <ApolloProvider client={apollo}>
+          <MainLayout>
+            <Component {...pageProps} />
+          </MainLayout>
+        </ApolloProvider>
+      </Web3ReactProvider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
