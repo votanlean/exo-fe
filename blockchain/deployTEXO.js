bc80bef569a34ceaeea80411abf3d9ca5f9d38de
Huy Tran <huy.tq2106@gmail.com>
2021-05-31 12:51:04 +0700

    feat: finish add script for deployment

--- blockchain/deployTEXO.js
+++ blockchain/deployTEXO.js
@@ -1,4 +1,11 @@
-require('dotenv').config(); // Load .env
+const path = require('path');
+
+const isProd = process.env.NODE_ENV === 'prod';
+
+require('dotenv').config({
+  path: path.resolve(__dirname, '../', isProd ? 'prod.env' : 'dev.env'),
+});
+
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const compiledTEXOToken = require('./build/TEXOToken.json');
@@ -7,25 +14,32 @@ const provider = new HDWalletProvider(
   process.env.MNEMONIC,
   process.env.BLOCKCHAIN_HOST
 );
+
 const web3 = new Web3(provider);
 
 // Initialization
 const bytecode = compiledTEXOToken.evm.bytecode.object;
 const abi = compiledTEXOToken.abi;
 
-
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
+  try {
+    const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+    console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(abi)
-    .deploy({
-        data: bytecode
-      })
-    .send({ gas: '3000000', from: accounts[0] });
+    const result = await new web3.eth.Contract(abi)
+      .deploy({
+          data: bytecode
+        })
+      .send({ gas: '3000000', from: accounts[0] });
 
-  const address = result.options.address;
-  console.log('Contract deployed to', address);
+    const address = result.options.address;
+    console.log('Contract deployed to', address);
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
+
 deploy();
