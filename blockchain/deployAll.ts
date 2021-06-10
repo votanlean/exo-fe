8f06d2cb1343194c5ba9b86791bba40f010b18d0
Huy Tran <huy.tq2106@gmail.com>
2021-06-10 15:15:09 +0700

    feat: deploy all script

---
+++ blockchain/deployAll.ts
@@ -0,0 +1,34 @@
+import deployTEXO from './deployTEXO';
+import deployTEXOOrchestrator from './deployOrchestrator';
+import deploySeedingPools from './deploySeedingPools';
+import deployLpPools from './deployLpPools';
+import deployFAANG from './deployFAANG';
+import deployFAANGOrchestrator from './deployFAANGOrchestrator';
+import deployFAANGPools from './deployFAANGPools';
+
+async function deployAll() {
+  try {
+    const texoAddress = await deployTEXO();
+    console.log('Contract tEXO deployed to', texoAddress);
+
+    const tEXOOrchestratorAddress = await deployTEXOOrchestrator(texoAddress);
+    console.log('Orchestrator contract deployed to', tEXOOrchestratorAddress);
+
+    await deploySeedingPools(tEXOOrchestratorAddress);
+    await deployLpPools(tEXOOrchestratorAddress);
+
+    const faangAddress = await deployFAANG();
+    console.log('Contract FAANG deployed to', faangAddress);
+
+    const faangOrchestratorAddress = await deployFAANGOrchestrator(faangAddress);
+    console.log('Contract FAANG Orchestrator deployed to', faangOrchestratorAddress);
+
+    await deployFAANGPools(faangOrchestratorAddress, faangAddress);
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+deployAll();
--- blockchain/deployLpPools.ts
+++ blockchain/deployLpPools.ts
@@ -1,5 +1,4 @@
 require('dotenv').config();
-console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
 import HDWalletProvider from 'truffle-hdwallet-provider';
 import Web3 from 'web3';
@@ -14,21 +13,20 @@ const web3 = new Web3(provider);
 
 // Initialization
 const abi = compiledOrchestrator.abi;
-const orchestratorAddress = process.env.ORCHESTRATOR_ADDRESS;
+const envOrchestratorAddress = process.env.ORCHESTRATOR_ADDRESS;
 const blockToUnlockClaimingRewards = process.env.BLOCK_TO_UNLOCK_CLAIMING_REWARDS;
 const ownerAddress = process.env.OWNER_ADDRESS;
 const chainId = process.env.CHAIN_ID;
 
-const deploy = async () => {
+export default async (deployedOrchestratorAddress = '') => {
   try {
-    const orchestratorContract = new web3.eth.Contract(abi as any, orchestratorAddress);
-    console.log('Attempting to deploy seeding pools from account', ownerAddress);
+    const orchestratorContract = new web3.eth.Contract(abi as any, deployedOrchestratorAddress || envOrchestratorAddress);
 
     for (let i = 0; i < lpPools.length; i++) {
       const lpPool = lpPools[i];
       console.log('Begin deploy LP pool:', lpPool.symbol);
 
-      const txHash = await orchestratorContract.methods
+      await orchestratorContract.methods
         .add(
           '0',
           lpPool.address[chainId],
@@ -43,16 +41,12 @@ const deploy = async () => {
           gas: '3000000',
         });
 
-      console.log('Successfully added LP pool:', txHash);
+      console.log('Successfully added LP pool:', lpPool.symbol);
       console.log('========================================');
     }
-
-    process.exit(0);
   } catch (error) {
     console.log(error);
 
     process.exit(1);
   }
 };
-
-deploy();
