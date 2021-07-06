71d2d97fa2be9d82ac56c335318f707aecc3fd21
An Vo <votanlean@gmail.com>
2021-07-06 11:31:55 +0700

    [Fix]: Fix harvest function

--- components/PoolActions/ClaimRewardsAction.tsx
+++ components/PoolActions/ClaimRewardsAction.tsx
@@ -9,15 +9,23 @@ import { useStyles } from './styles';
 function ClaimRewardsAction(props: any) {
   const classes = useStyles();
   const { disabled, data } = props || {};
-  const { orchestratorContract, id } = data || {};
+  const { orchestratorContract, id, refStake } = data || {};
 
   const { onReward, isLoading } = useHarvest(orchestratorContract, id);
+  const handleClaimReward = async () => {
+    let ref;
+
+    if (refStake) {
+      ref = '0x0000000000000000000000000000000000000000';
+    }
+    await onReward(ref);
+  };
 
   return (
     <Box>
       <Button
         className={classes.button}
-        onClick={onReward}
+        onClick={handleClaimReward}
         disabled={isLoading || disabled}
       >
         Claim Rewards
