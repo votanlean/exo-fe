2c4c125e8b858bd81dde535d5fd5c0ddb1d86f68
An Vo <votanlean@gmail.com>
2021-07-11 19:04:21 +0700

    Add BSC test-live

--- config/constants/farms.ts
+++ config/constants/farms.ts
@@ -8,7 +8,7 @@ export const bnbFarms: any[] = [
     symbol: 'tEXO/BNB LP',
     title: 'tEXO/BNB LP',
     address: {
-      56: '0x572274F3f1a2d4016d85EB1BA2c4DA671805218e',
+      56: '0x47097FA3c60E3Da49f717D5D43b4331C936d10ee',
       97: '0x7Dc1917631e2DA181b4Fd355d590125DeC8C71dA',
     },
     token: tokens.texo,
@@ -28,7 +28,7 @@ export const bnbFarms: any[] = [
     title: 'tEXO/BUSD LP',
     lpSymbol: 'tEXO-BUSD LP',
     address: {
-      56: '0x19F4F3Cdaae6923b387566161a10Dc517a0D11aF',
+      56: '0x849bf0733FF71977B4B1871Ae46a73C3Dbc37Cae',
       97: '0x62F36aA22a5DA1c8cD61A004eD7f17a6029B6539',
     },
     token: tokens.texo,
