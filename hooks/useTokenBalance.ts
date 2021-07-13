0bc8c662c77709a984dd64323e6c499d4ae9f4ed
An Vo <votanlean@gmail.com>
2021-07-13 22:11:25 +0700

    fix appNet walletNet inconsistency

--- hooks/useTokenBalance.ts
+++ hooks/useTokenBalance.ts
@@ -4,19 +4,29 @@ import { useWeb3React } from '@web3-react/core'
 import { BIG_ZERO } from 'utils/bigNumber'
 import useRefresh from './useRefresh'
 import {useERC20, useTEXOContract} from "./useContract";
+import { useNetwork } from 'state/hooks'
 
 const useTokenBalance = (tokenAddress: string) => {
   const [balance, setBalance] = useState(BIG_ZERO)
-  const { account } = useWeb3React()
+  const {id: appNetwork} = useNetwork();
+  const { account, library } = useWeb3React()
   const { fastRefresh } = useRefresh()
   const tokenContract = useERC20(tokenAddress);
 
   useEffect(() => {
     const fetchBalance = async () => {
-      const res = await tokenContract.methods.balanceOf(account).call()
-      setBalance(new BigNumber(res))
-    }
 
+      //check if the App Network and the Wallet Network is consistent
+      const walletNetwork = library?.networkVersion;
+      const isNetworkConsistent = Boolean(appNetwork == walletNetwork);
+
+      if(isNetworkConsistent){
+        const res = await tokenContract.methods.balanceOf(account).call()
+        setBalance(new BigNumber(res))
+      } else{
+        setBalance(new BigNumber(null))
+      }
+    }
     if (account) {
       fetchBalance()
     }
