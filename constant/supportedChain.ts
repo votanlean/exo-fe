ee1d32d9d6aaa84db0d3d5f3616e37217b4e02bf
exodex <thanh@focusgroup.asia>
2021-08-19 00:18:15 +0700

    change matic network rpc

--- constant/supportedChain.ts
+++ constant/supportedChain.ts
@@ -1,13 +1,13 @@
 export const supportedChains = [{
-    chainId: 56,
-    rpc: 'https://bsc-dataseed.binance.org/'
+  chainId: 56,
+  rpc: 'https://bsc-dataseed.binance.org/'
 }, {
-    chainId: 97,
-    rpc: 'https://data-seed-prebsc-1-s1.binance.org:8545'
+  chainId: 97,
+  rpc: 'https://data-seed-prebsc-1-s1.binance.org:8545'
 }, {
-    chainId: 137,
-    rpc: 'https://rpc-mainnet.matic.quiknode.pro'
+  chainId: 137,
+  rpc: 'https://rpc-mainnet.maticvigil.com'
 }, {
-    chainId: 80001,
-    rpc: 'https://rpc-mumbai.matic.today'
+  chainId: 80001,
+  rpc: 'https://rpc-mumbai.matic.today'
 }]
