8776212f9a107c9c519479184aec263b6b9ec2f5
An Vo <votanlean@gmail.com>
2021-08-19 07:24:12 +0700

    Add dapp plan polygon

--- constant/supportedChain.ts
+++ constant/supportedChain.ts
@@ -6,8 +6,8 @@ export const supportedChains = [{
   rpc: 'https://data-seed-prebsc-1-s1.binance.org:8545'
 }, {
   chainId: 137,
-  rpc: 'https://rpc-mainnet.maticvigil.com'
+  rpc: 'https://rpc-mainnet.maticvigil.com/v1/e5cf98dd4b3c2e17a1e632cf66d5eb98d3a9b6dd'
 }, {
   chainId: 80001,
-  rpc: 'https://rpc-mumbai.matic.today'
+  rpc: 'https://rpc-mumbai.maticvigil.com/v1/e5cf98dd4b3c2e17a1e632cf66d5eb98d3a9b6dd'
 }]
