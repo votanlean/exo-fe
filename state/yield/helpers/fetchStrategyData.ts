49e44e270d32d4260f55282ad910e7ed30b44913
An Vo <votanlean@gmail.com>
2021-09-05 22:16:41 +0700

    Add info config vaults

--- state/yield/helpers/fetchStrategyData.ts
+++ state/yield/helpers/fetchStrategyData.ts
@@ -5,6 +5,7 @@ import BigNumber from 'bignumber.js';
 import { BIG_TEN, normalizeTokenDecimal } from 'utils/bigNumber';
 import erc20 from 'config/abi/erc20.json';
 import masterchief from 'config/abi/masterchef.json';
+import TEXOOrchestrator from 'config/abi/TEXOOrchestrator.json';
 import pair from 'config/abi/Pair.json';
 
 const getPancakeswapStrategyData = async (strategy: any, chainId: number) => {
@@ -90,8 +91,91 @@ const getPancakeswapStrategyData = async (strategy: any, chainId: number) => {
   }
 }
 
+const getOrchestratorStrategyData = async (strategy: any, chainId:number)=>{
+  const { rewardPool, pool } = strategy;
+  const rewardPoolAddress = getAddress(rewardPool.address, chainId);
+  const poolId = getAddress(pool.id, chainId);
+
+  const masterchiefCalls = [
+    {
+      address: rewardPoolAddress,
+      name: 'poolInfo',
+      params: [poolId]
+    },
+    {
+      address: rewardPoolAddress,
+      name: 'totalAllocPoint',
+    },
+    {
+      address: rewardPoolAddress,
+      name: 'tEXOPerBlock',
+    },
+  ];
+
+  const [
+    poolInfo,
+    totalAllocPoint,
+    tEXOPerBlock
+  ] = await multicallRetry(TEXOOrchestrator, masterchiefCalls, chainId);
+
+  const [
+    [quoteTokenAddress]
+  ] = await multicallRetry(pair, [{
+    address: poolInfo.lpToken,
+    name: "token1"
+  }], chainId)
+
+  const erc20Calls = [
+    // Balance of quote token on LP contract
+    {
+      address: quoteTokenAddress,
+      name: 'balanceOf',
+      params: [poolInfo.lpToken],
+    },
+    // Balance of LP tokens in the master chef contract
+    {
+      address: poolInfo.lpToken,
+      name: 'balanceOf',
+      params: [rewardPoolAddress],
+    },
+    // Total supply of LP tokens
+    {
+      address: poolInfo.lpToken,
+      name: 'totalSupply',
+    },
+    // Quote token decimals
+    {
+      address: quoteTokenAddress,
+      name: 'decimals',
+    },
+  ];
+
+  const [quoteTokenBalanceLP, lpTokenBalanceMC, lpTotalSupply, quoteTokenDecimals] =
+  await multicallRetry(erc20, erc20Calls, chainId);
+
+  const lpTokenRatio = new BigNumber(lpTokenBalanceMC).div(new BigNumber(lpTotalSupply))
+  const quoteTokenAmountTotal = new BigNumber(quoteTokenBalanceLP).div(BIG_TEN.pow(quoteTokenDecimals))
+  const quoteTokenAmountMc = quoteTokenAmountTotal.times(lpTokenRatio)
+  const lpTotalInQuoteToken = quoteTokenAmountMc.times(new BigNumber(2))
+
+  return {
+    ...strategy,
+    rewardPool,
+    pool: {
+      ...pool,
+      lpToken: poolInfo.lpToken,
+      allocPoint: poolInfo.allocPoint.toNumber(),
+      lpTotalInQuoteToken: lpTotalInQuoteToken.toJSON(),
+      quoteTokenAddress,
+    },
+    totalAllocPoint: new BigNumber(totalAllocPoint[0]._hex).toNumber(),
+    cakePerBlock: normalizeTokenDecimal(new BigNumber(tEXOPerBlock)).toNumber(),
+  }
+}
+
 const strategyIndex = {
   [STRATEGY_TYPES.PANCAKESWAP]: getPancakeswapStrategyData,
+  [STRATEGY_TYPES.TEXO]: getOrchestratorStrategyData
 }
 
 export default async function fetchStrategyData(
@@ -102,8 +186,9 @@ export default async function fetchStrategyData(
     const { type, ...strategy } = yieldFarm.strategy;
 
     try {
-      const strategyData = await getPancakeswapStrategyData(strategy, chainId);
-      // const strategyData = strategyIndex[type] ? await strategyIndex[type](strategy, chainId) : {};
+      // const strategyData = await getPancakeswapStrategyData(strategy, chainId);
+      const strategyData = strategyIndex[type] ? await strategyIndex[type](strategy, chainId) : {};
+      console.log("strategyData: ", strategyData);
 
       return {
         ...yieldFarm,
