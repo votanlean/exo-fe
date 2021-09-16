e19f8679ea50cc9c05e623dbf9a92db45c42aeee
exodex <thanh@focusgroup.asia>
2021-09-16 22:33:56 +0700

    fix TLV missing yield farm value

--- state/farms/selectors.ts
+++ state/farms/selectors.ts
@@ -1,15 +1,16 @@
+import { useEffect, useState } from 'react'
 import BigNumber from "bignumber.js";
 import { BSC_FARM_ID, POLYGON_FARM_ID } from "constant/farms";
 import { useSelector } from "react-redux";
 import { useFarmQuoteTokenPrice, useTexoTokenPrice } from "state/texo/selectors";
 import tokens from "config/constants/tokens";
 import { useNetwork } from "state/hooks";
-import { gql, useQuery } from '@apollo/client'
 import { usePools } from "state/pools/selectors";
 import { useAppPrices } from "state/prices/selectors";
 import { getAddress } from 'utils/addressHelpers';
 import { getDecimals } from 'utils/decimalsHelper';
 import { useFAANGPools } from "state/fAANGpools/selectors";
+import { useYieldFarmsData } from 'state/yield/selector';
 import { normalizeTokenDecimal } from "utils/bigNumber";
 
 export const useFarmFromPid = (pid): any => {
@@ -33,7 +34,10 @@ export const useTotalValue = (): BigNumber => {
   const { id: chainId } = useNetwork();
   const pools = usePools();
   const fAANGs = useFAANGPools();
+  const yieldFarms = useYieldFarmsData();
   const allTokenPrices = useAppPrices();
+
+  const [yieldFarmsValue, setYieldFarmValue] = useState(0)
 
   let farmId: number;
 
@@ -91,123 +95,31 @@ export const useTotalValue = (): BigNumber => {
     const val = totalStaked.times(texoPrice)
     value = value.plus(val)
   })
-  return value;
-}
 
-const POLYGON_QUERY = ({
-  USDCPair,
-  wMATICPair
-}) => {
-  let gqlString = `
-        bundle(id: 1) {
-            id
-            ethPrice
-        }
+  useEffect(() => {
+    let total = new BigNumber(0)
 
-    `
+    yieldFarms.forEach((yieldFarm) => {
+      const { lpTotalInQuoteTokenVault } = yieldFarm
 
-  if (wMATICPair) {
-    gqlString += `
-        wMATICPair: pair(id: "${wMATICPair}") {
-            id
-            token0 {
-                id
-                symbol
-                name
-                derivedETH
-            }
-            token1 {
-                id
-                symbol
-                name
-                derivedETH
-            }
-            reserve0
-            reserve1
-        }
+      let stakingTokenPrice = 0
 
-        `
-  }
+      if (allTokenPrices.data) {
+        stakingTokenPrice =
+          allTokenPrices.data[
+          getAddress(yieldFarm.underlying.quote.address, chainId)?.toLowerCase()
+          ];
+      }
 
-  if (USDCPair) {
-    gqlString += `
-        USDCPair: pair(id: "${USDCPair}") {
-            id
-            token0 {
-                id
-                symbol
-                name
-                derivedETH
-            }
-            token1 {
-                id
-                symbol
-                name
-                derivedETH
-            }
-            reserve0
-            reserve1
-        }
+      const totalPriceUnderlyingDeposit = (new BigNumber(lpTotalInQuoteTokenVault)).times(new BigNumber(stakingTokenPrice));
 
-        `
-  }
-  return gql`
-        query POLYGON_QUERY {
-            ${gqlString}
-        }
-    `;
-}
+      total = total.plus(totalPriceUnderlyingDeposit)
+    })
 
-export const usePolygonTotalValue = (): BigNumber => {
-  let value = new BigNumber(0);
+    setYieldFarmValue(total.toNumber())
+  }, [yieldFarms])
 
-  const { id: chainId } = useNetwork();
-  const farms = useFarms();
-
-  const { data, loading } = useQuery(POLYGON_QUERY({
-    USDCPair: farms?.[0]?.address?.[chainId],
-    wMATICPair: farms?.[1]?.address?.[chainId],
-  }))
-
-  if (loading) {
-    return value;
-  }
-
-  if (chainId !== 80001 && chainId !== 137) {
-    return value;
-  }
-
-  if (data.USDCPair) {
-    const ethPrice = new BigNumber(data.bundle.ethPrice);
-
-    if (data.USDCPair.token0.derivedETH) {
-      const reserve = new BigNumber(data.USDCPair.reserve0);
-      const priceByEth = new BigNumber(data.USDCPair.token0.derivedETH)
-
-      value = value.plus(reserve.times(priceByEth).times(ethPrice));
-    } else if (data.USDCPair.token1.derivedETH) {
-      const reserve = new BigNumber(data.USDCPair.reserve1);
-      const priceByEth = new BigNumber(data.USDCPair.token1.derivedETH)
-
-      value = value.plus(reserve.times(priceByEth).times(ethPrice));
-    }
-  }
-
-  if (data.wMATICPair) {
-    const ethPrice = new BigNumber(data.bundle.ethPrice);
-
-    if (data.wMATICPair.token0.derivedETH) {
-      const reserve = new BigNumber(data.wMATICPair.reserve0);
-      const priceByEth = new BigNumber(data.wMATICPair.token0.derivedETH)
-
-      value = value.plus(reserve.times(priceByEth).times(ethPrice));
-    } else if (data.wMATICPair.token1.derivedETH) {
-      const reserve = new BigNumber(data.wMATICPair.reserve1);
-      const priceByEth = new BigNumber(data.wMATICPair.token1.derivedETH)
-
-      value = value.plus(reserve.times(priceByEth).times(ethPrice));
-    }
-  }
+  value = value.plus(yieldFarmsValue)
 
   return value;
 }
